9995f3083d5fe917e2a90935d02458d07189681f
school system <osundaman@gmail.com>
2026-05-18 10:42:31 +0000

    update env and migrations

--- supabase/migrations/20260517094329_bd66b5fb-869a-47d5-b7ff-f56921aff99d.sql
+++ supabase/migrations/20260517094329_bd66b5fb-869a-47d5-b7ff-f56921aff99d.sql
@@ -1,4 +1,2 @@
-UPDATE auth.users
-SET encrypted_password = crypt('Melton@2026!', gen_salt('bf')),
-    updated_at = now()
-WHERE lower(email) = 'meltongraymond1@gmail.com';
+-- skipped: handled manually
+SELECT 1;
