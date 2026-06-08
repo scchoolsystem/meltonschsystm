8fc74281c7b9a50ce83bfea4e4a428d975c18772
school system <osundaman@gmail.com>
2026-06-08 21:02:33 +0000

    Revert "Merge pull request #3 from scchoolsystem/mobile-desktop-development"
    This reverts commit 758447a1530f299dc17fd8cbefce36464c1b5dd1, reversing
    changes made to 712f17d4b4d53142386aaf38661d85c687c47916.

--- scripts/prepare-deploy.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-const base = JSON.parse(fs.readFileSync("wrangler.jsonc", "utf8"));
-base.main = "./server.js";
-base.assets = { directory: "../client", binding: "ASSETS" };
-fs.writeFileSync("dist/server/wrangler.json", JSON.stringify(base, null, 2));
