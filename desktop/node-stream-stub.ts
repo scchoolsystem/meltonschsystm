c2d38d7904ad87349bdc33d15e1a4b40c9fb055f
school system <osundaman@gmail.com>
2026-06-20 13:42:29 +0300

    Add Writable, PassThrough, and Transform classes

--- desktop/node-stream-stub.ts
+++ desktop/node-stream-stub.ts
@@ -1,2 +1,5 @@
 export class Readable {}
-export default { Readable };
+export class Writable {}
+export class PassThrough extends Writable {}
+export class Transform extends PassThrough {}
+export default { Readable, Writable, PassThrough, Transform };
