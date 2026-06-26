bce19e6e09ca4b831176274eff5ab5f6cb54e1ec
school system <osundaman@gmail.com>
2026-06-27 00:29:17 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -10,6 +10,7 @@ function createSupabaseClient() {
       storage: typeof window !== 'undefined' ? localStorage : undefined,
       persistSession: true,
       autoRefreshToken: true,
+      storageKey: 'sb-vpikrrytxeyybfhnozyt-auth-token',
     }
   });
 }
