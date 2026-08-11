6399a093ffb8f2092df128b061122b70d58b3822
school system <osundaman@gmail.com>
2026-08-11 19:29:43 +0000

    Revert "Add files via upload"
    This reverts commit ba6cff8b1d303cd953a3397d4784f80b93deb627.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+﻿import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  base: '/ojwangmariam/',
-  plugins: [react(), tailwindcss()],
-})
+  tanstackStart: {
+    server: { entry: "server" },
+  },
+});
